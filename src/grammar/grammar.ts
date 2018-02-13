16eb14663b75cfb2d11a3bd90c4b13c92c397310
Jason Dent <jason@streetsidesoftware.nl>
2018-02-13 21:35:40 +0100

    Add a Grammar endpoint.

--- src/grammar/grammar.ts
+++ src/grammar/grammar.ts
@@ -1,14 +1,41 @@
-// import { GrammarDefinition, Pattern, RegexOrString } from './grammarDefinition';
-// import { isPatternInclude, isPatternMatch, isPatternBeginEnd, scope } from './pattern';
-// import { XRegExp } from 'xregexp';
-// import { TokenizerState } from './tokenize';
+import { GrammarDefinition } from './grammarDefinition';
+import { Token, tokenizeLine, Rule } from './tokenize';
+import * as fs from 'fs-extra';
 
-// const maxDepth = 10;
+export interface Tokenizer {
+    tokenize(line: string): Token[];
+}
 
-// export class Grammar {
-//     constructor(private grammarDef: GrammarDefinition) {}
+export class Grammar {
+    constructor(private grammarDef: GrammarDefinition) {}
 
-//     tokenizeLine(line: string, state?: TokenizerState) {
-//     }
-// }
+    tokenizer(): Tokenizer {
+        let rule = grammarToRule(this.grammarDef);
 
+        return {
+            tokenize: (line: string) => {
+                const r = tokenizeLine(line, rule);
+                rule = r.state;
+                return r.tokens;
+            }
+        };
+    }
+
+    static async createFromFile(filename: string): Promise<Grammar> {
+        const json = await fs.readFile(filename, 'utf8');
+        const grammarDef = JSON.parse(json) as GrammarDefinition;
+        return new Grammar(grammarDef);
+    }
+}
+
+function grammarToRule(grammar: GrammarDefinition): Rule {
+    const rule: Rule = {
+        parent: undefined,
+        pattern: grammar,
+        scope: grammar.scopeName,
+        grammarDef: grammar,
+        depth: 0,
+    };
+
+    return rule;
+}
